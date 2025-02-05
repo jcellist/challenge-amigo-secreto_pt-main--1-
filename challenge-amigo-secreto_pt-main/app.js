80d417f16efe81ebe08aae7128ce94f42fcfe682
Joao Paulo <jpvioloncelo@gmail.com>
2025-02-05 09:55:28 -0300

    Amigo-secreto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,37 @@
+let amigos = [];
+let mensagemDeAlerta ='Por favor, insira um nome válido.';
+let listaDeAmigos = amigos
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value; 
+    if (nomeAmigo == '') {
+        return alert(`${mensagemDeAlerta}`);
+    }
+
+    amigos.push(nomeAmigo); 
+    console.log(amigos);
+    limparCampo();
+ }
+ function limparCampo(){
+    nomeAmigo = document.getElementById('amigo');
+    nomeAmigo.value = '';
+ }
+
+ function atualizarListaDeAmigos(){
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    listaDeAmigos.innerHTML = "";
+    for (let amigo of amigos) {
+        let item = document.createElement('li'); 
+        item.textContent = amigo; 
+        listaDeAmigos.appendChild(item); 
+    }
+}
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado');
+        if (amigos.length == 0) {
+            return resultado.innerHTML = 'Nenhum amigo disponível para sorteio!'; 
+            }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    resultado.innerHTML =`Amigo sorteado:${amigoSorteado}`;
+}
